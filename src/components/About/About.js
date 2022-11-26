fd2cdf82697bedfee09a97493703efdc6d138cd2
MG <magallardo11@outlook.com>
2022-11-25 19:31:00 -0700

    portfolio and about components

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,21 +1,26 @@
 import React from 'react'
+import mg from '../images/mg.png'
 
 const About = () => {
     return (
         <div className='aboutContainer text-white'>
             <div className='flex flex-col justify-center items-center '>
                 <div className=''>
-                    <p className='text-[35px]  text-blue-300 p-5'>
-                        About
+                    <p className='text-[35px] font-extrabold text-blue-300 p-5 flex justify-center'>
+                        About Me
                     </p>
                     <div></div>
                 </div>
+                <div>
+                    <img className=" rounded-full h-40 w-40 mx-auto" src={mg} />
+                </div>
                 <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                     <div className='text-center text-[25px]'>
-                        <p>Hello, my name is Miguel Gallardo. Navigate to the specified locations to get to know me more.</p>
+                        <p>Hello, my name is Miguel Gallardo. I am a 22 year old aspiring full-stack web developer profiecient/certified in the MERN stack. Not to mention, I recieved my Bachelors of Science in Psychology from Arizona State University.</p>
                     </div>
                     <div className='text-center text-[25px]'>
-                        <p>I am a 22 year old aspiring full-stack web developer profiecient/certified in the MERN stack. I recieved my B.S. in </p>
+                        <p> Skilled in working on several projects simultaneously, respectful and coachable team member. Detail oriented with a knack for solving problems. Open minded to new ideas and willing to put ego aside to complete assigned tasks.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -23,7 +28,7 @@ const About = () => {
 
             </div>
         </div>
-    
+
     );
 };
 
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import api from '../images/api.png'
+import booksearchengine from '../images/booksearchengine.png'
+import ecommerce from '../images/ecommerce.png'
+import forecast from '../images/forecast.png'
+import notetaker from '../images/notetaker.png'
+import PasswordGenerator from '../images/PasswordGenerator.png'
+import rankedify from '../images/rankedify.png'
+import Readmegenerator from '../images/Readmegenerator.png'
+import teamdisplaygenerator from '../images/teamdisplaygenerator.png'
+import wingman from '../images/wingman.png'
+import Workday from '../images/Workday.png'
 
 const Portfolio = () => {
     return (
         <div className='skillsContainer flex justify-center align-items mx-auto'>
-            <div className='imgContainer grid grid-cols-2'>
+            <div className='imgContainer grid grid-cols-3'>
                 <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
-                    <a href="#">
-                        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                    <a href=''>
+                        <img className="rounded-t-lg" src={rankedify} alt="" />
                     </a>
                     <div className="p-5">
                         <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Group Project 3: OnlyDevs</h5>
                         </a>
                         <p className="mb-3 font-normal text-black ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                         <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
@@ -21,14 +32,14 @@ const Portfolio = () => {
                 </div>
                 <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
                     <a href="#">
-                        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                        <img className="rounded-t-lg" src={rankedify} alt="" />
                     </a>
                     <div className="p-5">
                         <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Group Project 2: Rankedify</h5>
                         </a>
                         <p className="mb-3 font-normal text-black ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                        <a href="https://rankedify.herokuapp.com/" target='_blank' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                             Read more
 
                         </a>
@@ -36,14 +47,14 @@ const Portfolio = () => {
                 </div>
                 <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
                     <a href="#">
-                        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                        <img className="rounded-t-lg" src={wingman} alt="" />
                     </a>
                     <div className="p-5">
                         <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Group Project 1: WingMan</h5>
                         </a>
                         <p className="mb-3 font-normal text-black ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                        <a href="https://dustinm5oly.github.io/WingMan/" target='_blank' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                             Read more
 
                         </a>
@@ -51,14 +62,14 @@ const Portfolio = () => {
                 </div>
                 <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
                     <a href="#">
-                        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                        <img className="rounded-t-lg" src={booksearchengine} alt="" />
                     </a>
                     <div className="p-5">
                         <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Book Search Engine</h5>
                         </a>
                         <p className="mb-3 font-normal text-black ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                        <a href="https://miguel-book-search-engine.herokuapp.com/" target='_blank' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                             Read more
 
                         </a>
@@ -66,14 +77,14 @@ const Portfolio = () => {
                 </div>
                 <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
                     <a href="#">
-                        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                        <img className="rounded-t-lg" src={api} alt="" />
                     </a>
                     <div className="p-5">
                         <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Social Network API</h5>
                         </a>
                         <p className="mb-3 font-normal text-black ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                        <a href="https://github.com/magallardo77/Social-Network-API"  target='_blank' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                             Read more
 
                         </a>
@@ -81,14 +92,14 @@ const Portfolio = () => {
                 </div>
                 <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
                     <a href="#">
-                        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                        <img className="rounded-t-lg" src={ecommerce} alt="" />
                     </a>
                     <div className="p-5">
                         <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">E-Commerce Back-End</h5>
                         </a>
                         <p className="mb-3 font-normal text-black ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                        <a href="https://github.com/magallardo77/e-commerce-back-end" target='_blank' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                             Read more
 
                         </a>
@@ -96,14 +107,14 @@ const Portfolio = () => {
                 </div>
                 <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
                     <a href="#">
-                        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                        <img className="rounded-t-lg" src={notetaker} alt="" />
                     </a>
                     <div className="p-5">
                         <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Notetaker</h5>
                         </a>
                         <p className="mb-3 font-normal text-black ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                        <a href="https://mg-notetaker.herokuapp.com/" target='_blank' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                             Read more
 
                         </a>
@@ -111,14 +122,14 @@ const Portfolio = () => {
                 </div>
                 <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
                     <a href="#">
-                        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                        <img className="rounded-t-lg" src={teamdisplaygenerator} alt="" />
                     </a>
                     <div className="p-5">
                         <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Team Display Generator</h5>
                         </a>
                         <p className="mb-3 font-normal text-black ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                        <a href="https://github.com/magallardo77/team-display-generator" target='_blank'className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                             Read more
 
                         </a>
@@ -126,14 +137,14 @@ const Portfolio = () => {
                 </div>
                 <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
                     <a href="#">
-                        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                        <img className="rounded-t-lg" src={Readmegenerator} alt="" />
                     </a>
                     <div className="p-5">
                         <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">ReadMe Generator</h5>
                         </a>
                         <p className="mb-3 font-normal text-black ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                        <a href="https://github.com/magallardo77/Readme-generator" target='_blank' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                             Read more
 
                         </a>
@@ -141,14 +152,14 @@ const Portfolio = () => {
                 </div>
                 <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
                     <a href="#">
-                        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                        <img className="rounded-t-lg" src={forecast} alt="" />
                     </a>
                     <div className="p-5">
                         <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Weather Generator</h5>
                         </a>
                         <p className="mb-3 font-normal text-black ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                        <a href="https://magallardo77.github.io/Weather-Generator/" target='_blank' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                             Read more
 
                         </a>
@@ -156,14 +167,14 @@ const Portfolio = () => {
                 </div>
                 <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
                     <a href="#">
-                        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                        <img className="rounded-t-lg" src={Workday} alt="" />
                     </a>
                     <div className="p-5">
                         <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Daily Schedule</h5>
                         </a>
                         <p className="mb-3 font-normal text-black ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                        <a href="https://magallardo77.github.io/Daily-Scheduler/" target='_blank' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                             Read more
 
                         </a>
@@ -171,14 +182,14 @@ const Portfolio = () => {
                 </div>
                 <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md ">
                     <a href="#">
-                        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                        <img className="rounded-t-lg" src={PasswordGenerator} alt="" />
                     </a>
                     <div className="p-5">
                         <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Password Generator</h5>
                         </a>
                         <p className="mb-3 font-normal text-black ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                        <a href="https://magallardo77.github.io/Password-Generator/" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-sky-600 rounded-lg hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                             Read more
 
                         </a>
